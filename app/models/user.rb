56ab965f821fb3ae8c766f3a5153d57470f5e16d
kshabalin <nevskiylife@gmail.com>
2019-11-09 14:47:48 +0300

    update user model validations: email, password, password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
-  validates :password, length: { minimum: 8 }
-  validates :password, confirmation: { case_sensitive: true }
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/i }
+  validates :password, length: { minimum: 8 }, confirmation: true
+  validates :password_confirmation, presence: true
 
   has_many :links
 end
