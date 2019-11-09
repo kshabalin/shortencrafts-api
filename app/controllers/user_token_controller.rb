7252da606dc15e282a94781ffd90b51712672709
kshabalin <nevskiylife@gmail.com>
2019-11-09 15:14:07 +0300

    add user id to user token create action response

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,10 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
+
+  def create
+    render json: {
+        jwt: auth_token.token,
+        user_id: entity.id
+    }, status: :created
+  end
 end
