3c46c67e1b492c59d249d419b695cef355ac95f1
kshabalin <nevskiylife@gmail.com>
2019-11-09 15:14:43 +0300

    implement model specs

---
+++ spec/models/click_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Click, type: :model do
+  describe "Associations" do
+    it { should belong_to(:link) }
+  end
+
+  describe "Validations" do
+    let(:link) { Link.create(url: 'https://www.testlink.com/', url_hash: '06517b') }
+    let(:click_params) { {country: 'Russian Federation', country_code: 'RU'} }
+
+    it { should validate_presence_of(:link) }
+
+    it 'is valid with valid attributes' do
+      click = link.clicks.new(click_params)
+      expect(click).to be_valid
+    end
+
+    it 'is not valid without a username' do
+      click_params[:country] = nil
+      click = link.clicks.new(click_params)
+      expect(click).to_not be_valid
+    end
+
+    it 'is not valid without a email' do
+      click_params[:country_code] = nil
+      click = link.clicks.new(click_params)
+      expect(click).to_not be_valid
+    end
+  end
+end
