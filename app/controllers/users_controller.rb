426f3fa369b4f0c32d1f7eab8353353c1717518f
kshabalin <nevskiylife@gmail.com>
2019-11-09 15:29:55 +0300

    add errors to unsuccessful register user response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {status: 200, msg: 'User was created'}
+      render json: { msg: "User #{user.username} was created", succeeded: true }
+    else
+      render json: { msg: user.errors.full_messages.join("\n"), succeeded: false }
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
   end
 end
