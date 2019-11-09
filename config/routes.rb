a7c16c05e40cd0f30fb2ff375b245a0bfc731d10
kshabalin <nevskiylife@gmail.com>
2019-11-09 14:46:17 +0300

    move links routes into resources block

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   scope :api do
     get '/redirect', to: 'links#redirect'
     get '/redirect/:url_hash', to: 'links#redirect'
-    get '/links', to: 'links#index'
-    post '/links', to: 'links#create'
-    delete '/links/:id', to: 'links#destroy'
+    resources :links, only: [:index, :create, :destroy]
     get '/clicks', to: 'clicks#index'
     post '/signin', to: 'user_token#create'
     post '/signup', to: 'users#create'
